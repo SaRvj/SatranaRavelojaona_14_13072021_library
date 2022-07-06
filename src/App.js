8352401795798e77a210da40057eaad67ee5d0b8
Satrana Ravelojaona <satranaravelojaona@gmail.com>
2022-07-06 12:08:11 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Modal from './lib/Modal';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const toggleModal = () => setModalIsOpen(!modalIsOpen);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div onClick={toggleModal}>
+        Click here to open modal
+      </div>
+
+      <Modal isOpen={modalIsOpen} onConfirm={toggleModal}/>
     </div>
   );
 }
